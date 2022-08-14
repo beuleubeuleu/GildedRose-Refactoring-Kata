aa70cc394232e0a24be1ded14895d59f8bb1a47a
beuleu <billyklotz789@gmail.com>
2022-08-14 17:17:41 +0200

    code is restructured, will continue refactoring

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -13,56 +13,63 @@ class GildedRose(var items: Array<Item>) {
             val isAgedBrie = items[i].name == AGED_BRIE
             val isBackstage = items[i].name == BACKSTAGE
             val isSulfuras = items[i].name == SULFURAS
+            val isCommonItem = !isAgedBrie && !isBackstage && !isSulfuras
             fun qualityCanIncrease() = items[i].quality < 50
             fun qualityCanDecrease() = items[i].quality > 0
             fun sellIn11Days() = items[i].sellIn < 11
             fun sellIn6Days() = items[i].sellIn < 6
             fun noDaysToSell() = items[i].sellIn < 0
 
-            if (!isAgedBrie && !isBackstage) {
-                if (qualityCanDecrease()) {
-                    if (!isSulfuras) {
-                        items[i].quality --
-                    }
-                }
-            } else {
+
+            if (isAgedBrie) {
+
                 if (qualityCanIncrease()) {
-                    items[i].quality ++
-
-                    if (isBackstage) {
-                        if (sellIn11Days()) {
-                            if (qualityCanIncrease()) {
-                                items[i].quality ++
-                            }
-                        }
-
-                        if (sellIn6Days()) {
-                            if (qualityCanIncrease()) {
-                                items[i].quality ++
-                            }
-                        }
-                    }
+                    items[i].quality++
                 }
-            }
 
-            if (!isSulfuras) {
                 items[i].sellIn --
-            }
 
-            if (noDaysToSell()) {
-                if (!isAgedBrie) {
-                    if (!isBackstage) {
-                        if (qualityCanDecrease()) {
-                            if (!isSulfuras) {
-                                items[i].quality --
-                            }
-                        }
-                    } else {
-                        items[i].quality = 0
-                    }
-                } else {
+                if(noDaysToSell()) {
                     if (qualityCanIncrease()) {
                         items[i].quality ++
+                    }
+                }
+            }
+
+            if (isBackstage) {
+
+                items[i].quality ++
+
+                if (sellIn11Days()) {
+                    if (qualityCanIncrease()) {
+                        items[i].quality ++
+                    }
+                }
+
+                if (sellIn6Days()) {
+                    if (qualityCanIncrease()) {
+                        items[i].quality ++
+                    }
+                }
+
+                items[i].sellIn --
+
+                if(noDaysToSell()) {
+                    items[i].quality = 0
+                }
+            }
+
+            if(isCommonItem) {
+
+                if (qualityCanDecrease()) {
+                        items[i].quality --
+                }
+
+                items[i].sellIn --
+                
+                if(noDaysToSell()) {
+                    if (qualityCanDecrease()) {
+                        items[i].quality--
                     }
                 }
             }
